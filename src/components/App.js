9b912cbaf285ea6839c2952193e12d58f1a53de3
Prashil S Gupta <33949630+Prashil1999@users.noreply.github.com>
2020-11-07 19:16:26 +0530

    changes2

--- src/components/App.js
+++ src/components/App.js
@@ -11,50 +11,82 @@ class Timer extends React.Component {
       y: 0
     };
     this.handleClick = this.handleClick.bind(this);
+    this.componentDidUpdate = this.componentDidUpdate.bind(this);
     this.intervalId = 0;
   }
   componentDidMount() {
-    document.addEventListener("keydown", (event) => {
-      let x = this.state.x;
-      let y = this.state.y;
-      if (event.keyCode === 37) {
-        x -= 5;
-      } else if (event.keyCode === 38) {
-        y -= 5;
-      } else if (event.keyCode === 39) {
-        x += 5;
-      } else if (event.keyCode === 40) {
-        y += 5;
-      }
-      let stateCopy = this.state;
-      stateCopy.x = x;
-      stateCopy.y = y;
-      this.setState(stateCopy, () => {
-        let stateCopy = this.state;
-        stateCopy.class = { top: this.state.y, left: this.state.x };
-        this.setState(stateCopy);
-      });
-    });
+    //if (this.state.is_started)
+    // document.addEventListener("keydown", (event) => {
+    //   let x = this.state.x;
+    //   let y = this.state.y;
+    //   if (event.keyCode === 37) {
+    //     x -= 5;
+    //   } else if (event.keyCode === 38) {
+    //     y -= 5;
+    //   } else if (event.keyCode === 39) {
+    //     x += 5;
+    //   } else if (event.keyCode === 40) {
+    //     y += 5;
+    //   }
+    //   let stateCopy = this.state;
+    //   stateCopy.x = x;
+    //   stateCopy.y = y;
+    //   this.setState(stateCopy, () => {
+    //     let stateCopy = this.state;
+    //     stateCopy.class = { top: this.state.y, left: this.state.x };
+    //     this.setState(stateCopy);
+    //   });
+    // });
   }
 
+  componentDidUpdate() {
+    if (this.state.x === 250 && this.state.y === 250) {
+      clearInterval(this.intervalId);
+    }
+  }
   componentWillUnmount() {}
 
   handleClick() {
     let stateCopy = this.state;
     stateCopy.is_started = !this.state.is_started;
-    this.setState(stateCopy);
+    this.setState(stateCopy, () => {
+      if (this.state.is_started) {
+        document.addEventListener("keydown", (event) => {
+          let x = this.state.x;
+          let y = this.state.y;
+          if (event.keyCode === 37) {
+            x -= 5;
+          } else if (event.keyCode === 38) {
+            y -= 5;
+          } else if (event.keyCode === 39) {
+            x += 5;
+          } else if (event.keyCode === 40) {
+            y += 5;
+          }
+          let stateCopy = this.state;
+          stateCopy.x = x;
+          stateCopy.y = y;
+          this.setState(stateCopy, () => {
+            let stateCopy = this.state;
+            stateCopy.class = { top: this.state.y, left: this.state.x };
+            this.setState(stateCopy);
+          });
+        });
+        this.intervalId = setInterval(() => {
+          let stateCopy = this.state;
+          stateCopy.time++;
+          this.setState(stateCopy);
+        }, 1000);
+      }
+    });
   }
 
   render() {
     return (
       <>
-        {this.state.is_started ? (
-          <>
-            <div className="ball" style={this.state.class}></div>
-            <div className="hole"></div>
-            <div className="heading-timer">{this.state.time}</div>
-          </>
-        ) : null}
+        <div className="ball" style={this.state.class}></div>
+        <div className="hole"></div>
+        <div className="heading-timer">{this.state.time}</div>
         <button className="start" onClick={this.handleClick}>
           start
         </button>
